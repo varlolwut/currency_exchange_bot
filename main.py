161e735eae22e7a078ad57cb9f41cc1c803f163b
Aleksey.Bulygin <aleksey.bulygin@AlekseyBulygins-MacBook-Pro.local>
2022-07-29 19:17:22 +0300

    add allbanks.kz

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from urllib.parse import urlparse
 
 DATABASE = './exchange_rate.sql'
 CHROMEDRIVER = "./chromedriver"
-URL_LIST = ['https://sber.kz/', 'https://eubank.kz/exchange-rates/']
+URL_LIST = ['https://sber.kz/', 'https://eubank.kz/exchange-rates/', 'https://allbanks.kz/exchange_rates']
 
 if __name__ == '__main__':
-    df_list = [parse_sber(CHROMEDRIVER, URL_LIST[0]),parse_eubank(get_currency_rate_page(URL_LIST[1]))]
+    df_list = [parse_sber(CHROMEDRIVER, URL_LIST[0]), parse_eubank(get_currency_rate_page(URL_LIST[1])),
+               parse_allbanks(get_currency_rate_page(URL_LIST[2]))]
     for frame, url in zip(df_list, URL_LIST):
         sqlite_write(DATABASE, frame, urlparse(url).netloc.split(".", 1)[0])
         # print(frame.head())
--- parse.py
+++ parse.py
@@ -4,6 +4,8 @@ import requests
 from datetime import datetime
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 def get_currency_rate_page(currency_rate_url):
@@ -27,6 +29,7 @@ def parse_eubank(html_page):
                                zip(titles, values)])
     final_df = pd.DataFrame(sum(tmp_result, []))
     final_df['ts'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    final_df['bank_name'] = 'Евразийский Банк'
     # print(final_df.head())
     return final_df
 
@@ -47,8 +50,42 @@ def parse_sber(sel_driver, url):
                  'currency': 'RUB', 'rate': newstring[-1]}]
     final_df = pd.DataFrame(tmp_list)
     final_df['ts'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    final_df['bank_name'] = 'Сбербанк Казахстан'
     # print(final_df.head())
     driver.close()
     return final_df
+
+
+def parse_allbanks(html_page):
+    soup = bs(html_page.text, "html.parser")
+    table = soup.find('table', class_='top15')
+    df = pd.DataFrame(
+        columns=['bank_name', 'USD_Покупка', 'USD_Продажа', 'EUR_Покупка', 'EUR_Продажа', 'RUB_Покупка', 'RUB_Продажа'])
+    for row in table.tbody.find_all('tr'):
+        columns = row.find_all('td')
+        if (columns != []):
+            bank_name = columns[0].text
+            usd_buy = columns[1].text
+            usd_sell = columns[2].text
+            eur_buy = columns[3].text
+            eur_sell = columns[4].text
+            rub_buy = columns[5].text
+            rub_sell = columns[6].text
+            print()
+            df = df.append(
+                {'bank_name': bank_name, 'USD_Покупка': usd_buy, 'USD_Продажа': usd_sell, 'EUR_Покупка': eur_buy,
+                 'EUR_Продажа': eur_sell, 'RUB_Покупка': rub_buy, 'RUB_Продажа': rub_sell}, ignore_index=True)
+            # df = df.concat()
+    #         df['transaction_source'] = 'unspecified'
+    #         df['ts'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    df = df.melt(id_vars=["bank_name"],
+                 var_name="currency",
+                 value_name="rate")
+    df[['currency', 'operation_type']] = df['currency'].str.split('_', expand=True)
+    df['transaction_source'] = 'unspecified'
+    df['ts'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # print(df)
+    return df
+
 
 
