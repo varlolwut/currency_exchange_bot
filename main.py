6642de0852a6d0229ab12b2d3e5d9a3b024cbee1
Alexey Bulygin <30726976+varlolwut@users.noreply.github.com>
2023-03-21 16:53:20 +0300

    async

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from parse import *
-from sqlite_operator import *
+import asyncio
+from parse import parse_sber_async, parse_eubank_async, parse_allbanks_async
+from sqlite_operator import sqlite_write
 from urllib.parse import urlparse
 
 DATABASE = './exchange_rate.sql'
 CHROMEDRIVER = "./chromedriver"
 URL_LIST = ['https://sber.kz/', 'https://eubank.kz/exchange-rates/', 'https://allbanks.kz/exchange_rates']
 
+async def main():
+    async with aiohttp.ClientSession() as session:
+        df_list = [
+            await parse_sber_async(CHROMEDRIVER, URL_LIST[0], session),
+            await parse_eubank_async(session, URL_LIST[1]),
+            await parse_allbanks_async(session, URL_LIST[2])
+        ]
+        for frame, url in zip(df_list, URL_LIST):
+            frame['rate'] = frame['rate'].astype(float)
+            frame['ts'] = pd.to_datetime(frame['ts'])
+            sqlite_write(DATABASE, frame, urlparse(url).netloc.split(".", 1)[0])
+
 if __name__ == '__main__':
-    df_list = [parse_sber(CHROMEDRIVER, URL_LIST[0]), parse_eubank(get_currency_rate_page(URL_LIST[1])),
-               parse_allbanks(get_currency_rate_page(URL_LIST[2]))]
-    for frame, url in zip(df_list, URL_LIST):
-        frame['rate'] = frame['rate'].astype(float)
-        frame['ts'] = pd.to_datetime(frame['ts'])
-        sqlite_write(DATABASE, frame, urlparse(url).netloc.split(".", 1)[0])
-        # print(frame.head())
-        # print(url)
-        # print(urlparse(url).netloc.split(".", 1)[0])
+    asyncio.run(main())
