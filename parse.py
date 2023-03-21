2059a82e2f6af217926e2542e2fe2e4ce1b77b59
Alexey Bulygin <30726976+varlolwut@users.noreply.github.com>
2023-03-21 16:23:24 +0300

    async optimization

--- parse.py
+++ parse.py
@@ -1,79 +1,59 @@
-from bs4 import BeautifulSoup as bs
-import pandas as pd
-import requests
-from datetime import datetime
-from selenium.webdriver import Chrome
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import warnings
+import aiohttp
+import asyncio
 import re
-warnings.simplefilter(action='ignore', category=FutureWarning)
+from bs4 import BeautifulSoup as bs
+from datetime import datetime
+import pandas as pd
 
-#linux deps
-# from pyvirtualdisplay import Display
-# display = Display(visible=0, size=(800, 800))
-# display.start()
-# driver = webdriver.Chrome()
-
-
-ChromeOptions = Options()
-ChromeOptions.headless = True
-
-
-def get_currency_rate_page(currency_rate_url):
-    main_url = currency_rate_url
-    r = requests.get(main_url)
-    return r
-
+async def get_currency_rate_page(session, url):
+    async with session.get(url) as response:
+        return await response.text()
 
 def parse_eubank(html_page):
     non_decimal = re.compile(r'[^\d.]+')
-    soup = bs(html_page.text, "html.parser")
+    soup = bs(html_page, "html.parser")
     exchange_types_tmp = ['В обменных пунктах', 'В Smartbank', 'Для юридических лиц', 'Золотые слитки']
     exchange_types = [val for val in exchange_types_tmp for _ in (0, 1)]
     tmp_result = []
-    for tables, et in zip(soup.find_all('div', class_='exchange__col'), exchange_types):
-        for _ in tables.find_all('h3', class_='exchange__title'):
-            operation_type = tables.find('h3', class_='exchange__title')  # тип операции
-            titles = tables.find_all('span', class_='exchange-table__title')  # валюта
-            values = tables.find_all('span', class_='exchange-table__value')  # курс
-            tmp_result.append([{'transaction_source': et, 'transaction_type': operation_type.text,
-                                'currency': currency.text, 'rate': non_decimal.sub('', rate.text)} for currency, rate in
-                               zip(titles, values)])
-    final_df = pd.DataFrame(sum(tmp_result, []))
+    for tables, et in zip(soup.select('div.exchange__col'), exchange_types):
+        operation_type = tables.h3.text.strip()  # тип операции
+        titles = [title.text for title in tables.select('span.exchange-table__title')]  # валюта
+        values = [value.text for value in tables.select('span.exchange-table__value')]  # курс
+        tmp_result.extend([{'transaction_source': et, 'transaction_type': operation_type,
+                            'currency': currency, 'rate': non_decimal.sub('', rate)} for currency, rate in
+                           zip(titles, values)])
+    final_df = pd.DataFrame(tmp_result)
     final_df['ts'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     final_df['bank_name'] = 'Евразийский Банк'
-    # print(final_df.head())
-    # print(final_df.dtypes)
     return final_df
 
+async def parse_eubank_async(session, url):
+    html_page = await get_currency_rate_page(session, url)
+    return parse_eubank(html_page)
 
-def parse_sber(sel_driver, url):
-    count = 0
-    driver = Chrome(executable_path=sel_driver)
-    # driver = webdriver.Chrome(sel_driver)
-    # linux dep
-    driver.get(url)
-    news_element = driver.find_element(By.CLASS_NAME, "rates__table").text
-    words_to_list = news_element.split()
+async def parse_sber(sel_driver, url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            html_page = await response.text()
+            non_decimal = re.compile(r'[^\d.]+')
+            soup = bs(html_page, "html.parser")
+            news_element = soup.select('.rates__table')[0].text
+            words_to_list = news_element.split()
+            count = 0
+            for _ in words_to_list:
+                count = count + 1
+            newstring = words_to_list[0:2] + words_to_list[count - 2: count]
+            tmp_list = [{'transaction_source': 'unspecified', 'transaction_type': newstring[0],
+                         'currency': 'RUB', 'rate': newstring[-2]},
+                        {'transaction_source': 'unspecified', 'transaction_type': newstring[1],
+                         'currency': 'RUB', 'rate': newstring[-1]}]
+            final_df = pd.DataFrame(tmp_list)
+            final_df['ts'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            final_df['bank_name'] = 'Сбербанк Казахстан'
+            return final_df
 
-    for _ in words_to_list:
-        count = count + 1
-    newstring = words_to_list[0:2] + words_to_list[count - 2: count]
-    tmp_list = [{'transaction_source': 'unspecified', 'transaction_type': newstring[0],
-                 'currency': 'RUB', 'rate': newstring[-2]},
-                {'transaction_source': 'unspecified', 'transaction_type': newstring[1],
-                 'currency': 'RUB', 'rate': newstring[-1]}]
-    final_df = pd.DataFrame(tmp_list)
-    final_df['ts'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    final_df['bank_name'] = 'Сбербанк Казахстан'
-    # print(final_df.head())
-    driver.close()
-    return final_df
-
-
-def parse_allbanks(html_page):
-    soup = bs(html_page.text, "html.parser")
+async def parse_allbanks(html_page):
+    soup = bs(html_page, "html.parser")
     table = soup.find('table', class_='top15')
     df = pd.DataFrame(
         columns=['bank_name', 'USD_Покупка', 'USD_Продажа', 'EUR_Покупка', 'EUR_Продажа', 'RUB_Покупка', 'RUB_Продажа'])
@@ -87,21 +67,13 @@ def parse_allbanks(html_page):
             eur_sell = columns[4].text
             rub_buy = columns[5].text
             rub_sell = columns[6].text
-            print()
             df = df.append(
                 {'bank_name': bank_name, 'USD_Покупка': usd_buy, 'USD_Продажа': usd_sell, 'EUR_Покупка': eur_buy,
                  'EUR_Продажа': eur_sell, 'RUB_Покупка': rub_buy, 'RUB_Продажа': rub_sell}, ignore_index=True)
-            # df = df.concat()
-    #         df['transaction_source'] = 'unspecified'
-    #         df['ts'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     df = df.melt(id_vars=["bank_name"],
                  var_name="currency",
                  value_name="rate")
     df[['currency', 'operation_type']] = df['currency'].str.split('_', expand=True)
     df['transaction_source'] = 'unspecified'
     df['ts'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # print(df)
     return df
-
-
-
